e0f65470a2f6f606bd23079db05589f316f0b028
Wing <eamonn.kearns@so-4pt.net>
2019-04-01 04:57:24 +0100

    Nicer, though less efficient, code.

--- ObjectCache.js
+++ ObjectCache.js
@@ -1,6 +1,25 @@
 'use strict';
+const   MAX_TTL_DEFAULT = 60000,
+        md5 = require('md5');
 
-const MAX_TTL_DEFAULT = 60000;
+class ObjectCacheEntity
+{
+    constructor(entry)
+    {
+        this.entry = entry;
+        this.updateLastAccessed();
+    }
+
+    updateLastAccessed()
+    {
+        this.lastAccessed = Date.now();
+    }
+
+    get age()
+    {
+        return Date.now() - this.lastAccessed;
+    }
+}
 
 /**
  * A simple object cache
@@ -11,65 +30,108 @@ class ObjectCache
     {
         this.maxTTL = maxTTL && !isNaN(maxTTL)?parseInt(maxTTL):MAX_TTL_DEFAULT;
         this.cache = {};
-        setTimeout(()=>{this.checkCache();}, this.maxTTL);
+        this.timeout = setTimeout(()=>{this.checkCache();}, this.maxTTL);
+        this.size = 0;
     }
 
     /**
-     * @param The key by which to store the object
-     * @param The object to store
+     * Adds an item to the cache and sets the time it was last accessed at to now
+     * @param key The key by which to store the object, or an object you want to store. A hash will be created of the object, by its properties.
+     * @param object The object to store
+     * @return The key by which the item was stored in the cache
      */
     put(key, object)
     {
-        let now = Date.now();
-        if(this.cache[key])
+        if(key === undefined || object === undefined)
         {
-            this.cache[key].lastAccessed = now;
-            return;
+            throw new Error('Invalid number of arguments');
         }
 
-        this.cache[key] =  {
-            entry:object,
-            lastAccessed:now
-        };
+        if(this.cache[key])
+        {
+            this.cache[key].updateLastAccessed();
+            return key;
+        }
+
+        this.cache[key] = new ObjectCacheEntity(object);
+        this.size++;
+
+        return key;
+    }
+
+    add(object)
+    {
+        let key = md5(JSON.stringify(object));
+        return this.put(key, object);
     }
 
     /**
+     * Gets an item from the cache by its key and updates the time it was last accessed at
      * @param key
-     * @returns {null|the object in the cache at position key*}
+     * @returns The object in the cache for the key. Or null if no entry found.
      */
     get(key)
     {
-        let now = Date.now();
-        if(this.cache[key])
+        if(this.cache[key] !== null)
         {
-            return now;
-            this.cache[key].lastAccessed = now;
+            this.cache[key].updateLastAccessed();
             return this.cache[key].entry;
         }
         return null;
     }
 
+    /**
+     * Remove an item from the cache by its key and return it
+     * @param key
+     */
     remove(key)
     {
+        let object = this.cache[key].entry;
         if(this.cache[key])
         {
+            this.size --;
             delete this.cache[key];
         }
+        return object;
     }
 
+    clear()
+    {
+        this.cache = {};
+        this.size = 0;
+        return this;
+    }
+
+    destroy()
+    {
+        this.clear();
+        clearTimeout(this.timeout);
+    }
+
+    empty()
+    {
+        this.clear();
+        return this;
+    }
+
+    /**
+     * Check the cache. This is done every this.maxTTL milliseconds via a timeout
+     */
     checkCache()
     {
-        let now = Date.now(),
-            pruneTime = now - this.maxTTL;
-        for(let i in this.cache)
+        clearTimeout(this.timeout);
+        let removedItems = 0;
+
+        for (let i in this.cache)
         {
-            if(this.cache[i].lastAccessed < pruneTime)
+            if (this.cache[i].age > this.maxTTL)
             {
+                removedItems++;
                 delete this.cache[i];
             }
         }
-
-        setTimeout(()=>{this.checkCache();}, this.maxTTL);
+        this.size -= removedItems;
+        this.timeout = setTimeout(() => {this.checkCache();}, this.maxTTL);
     }
 
 }
