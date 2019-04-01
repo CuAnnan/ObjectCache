f216bccda836435078ad5cad2d6dc4794b46fc76
Wing <eamonn.kearns@so-4pt.net>
2019-04-01 05:29:52 +0100

    Optimised check method by using since instead of date.now for all entries in iteration. Introduces rare edge case of something passing its lifecycle.

---
+++ testCache.js
@@ -0,0 +1,34 @@
+let assert = require('assert');
+
+class SampleClass
+{
+    constructor(var1, var2)
+    {
+        this.var1 = var1;
+        this.var2 = var2;
+    }
+
+    toJSON()
+    {
+        return {
+            'var1':this.var1,
+            'var2':this.var2
+        };
+    }
+}
+
+const Cache = require('./ObjectCache');
+let cache = new Cache(1000),
+    sampleData = new SampleClass("test", "more Test"),
+    key1 = cache.add(sampleData),
+    key2 = cache.add(sampleData),
+    key3 = cache.add(sampleData),
+    sampleData2 = cache.get(key1);
+setTimeout(
+    ()=>{
+        let sampleData3 = new SampleClass('test', 'even More Test'),
+            key4 = cache.add(sampleData3);
+    },
+    500);
+
+
